e89df7489e9a263207bae58a7aa2d94d82561a1c
nycmouli <nycmouli@gmail.com>
2016-09-21 17:32:58 -0600

    First push. Basis solution fo !D quantum potential.
    Unit tests for bump, sho and and kronig penny model done.
    
    All tests passed.

---
+++ basis/solve.py
@@ -0,0 +1,64 @@
+#!/Users/nycmouli/anaconda/bin/python
+from basis import msg
+def examples():
+    """Prints examples of using the script to the console using colored output.                                                   
+    """
+    script = "Basis: 1D Quantum potential Solver via Basis Expansion."
+    explain = ("For simple 1D potentials such as Kronig penny. The code produces"
+               "numerical solutions.")
+    contents = [(("Solve the potetial in `kp.cfg` using 200 basis funtions"),
+                 "solve.py 200 -potential kp.cfg",
+                 "This solves the solution to the default `output.dat' "),
+                (("Solve the potetial `sho.cfg`, save the solution to "
+                  " `mysol.out`."),
+                 "solve.py 400 -potential sho.cfg -outfile mysol.out","")]
+    required = ("REQUIRED: potential config file `pot.cfg`")
+    output = ("RETURNS: plot window; for logging-only mode, the data being "
+              "logged is also periodically printed to stdout.")
+    details = ("The plotting uses `matplotlib` with the default configured "
+               "backend. If you want a different backend, set the rc config "
+               "for `matplotlib` using online documentation. However, many "
+               "backends don't play well with the animation (depending on OS "
+               "type and version, etc., etc.; so use carefully.")
+    outputfmt = ("")
+
+    msg.example(script, explain, contents, required, output, outputfmt, details)
+
+
+script_options = {
+    "N": dict(default="100",type=int,
+              help=("Specifies how many basis functions to use in the expansion solution.")),
+    "-plot": dict(action="store_true",
+                  help=("Plots the solution")),
+    "-autoconf": {"action": "store_true",
+                  "help": ("Runs in automatic mode by loading a "
+                           "configuration file in the current directory "
+                           "called 'sensors.cfg'.")},
+    "-config": {"help": ("Specify a configuration file to get sensor "
+                         "setup information from.")},
+    }
+"""dict: default command-line arguments and their                                                                                              
+    :meth:`argparse.ArgumentParser.add_argument` keyword arguments.                                                                            
+"""
+
+
+def _parser_options():
+    """Parses the options and arguments from the command line."""
+    #We have two options: get some of the details from the config file,                                                                        
+    import argparse
+    from basis import base
+    pdescr = "1D-qunatum solver"
+    parser = argparse.ArgumentParser(parents=[base.bparser], description=pdescr)
+    for arg, options in script_options.items():
+        parser.add_argument(arg, **options)
+
+    args = base.exhandler(examples, parser)
+    if args is None:
+        return
+    return args
+
+def run(args):
+    print("Running",args)
+
+if __name__ == '__main__': # pragma: no cover                                                                                     
+    run(_parser_options())
---
+++ tests/test_potential.py
@@ -0,0 +1,81 @@
+""" Tests the evaluation of the potential for single and array-valued arguments for SHO, bump and KP potentials
+"""
+import numpy as np
+import pytest
+from basis.potential import Potential
+
+
+
+def adjust(pot, **kwargs):
+    """Adjusts the parameters of the potential.
+
+    Args:
+    kwargs(dict: parameters and values to overwrite.)
+    """
+    self.params.update(kwargs)
+    self._parse_regions()
+
+
+def test_sho():
+    """Tests the SHO potential.
+    """
+    pot = Potential("potentials/sho.cfg")
+
+    params = [(2,1,-15,100),
+              (1e5,1e3,-1234.,1e5),
+              (1./3,1./6,-10,5),
+              (np.pi,np.pi/2., -np.sqrt(2),23)]
+
+    for a, shift, V0, N in params:
+        pot.adjust(a=a,shift=shift,v0=V0)
+        xa= np.linspace(-a,a,N)
+        assert pot(shift) == 0.
+        assert pot(shift/2.) == V0/4.*shift**2
+        assert len(pot(xa))== N
+        assert pot(a) == 0.
+        assert pot(-5.*a) == 0.
+        with pytest.raises(ValueError):
+         pot("a")
+
+
+
+def test_bump():
+    """Tests the bump in the square well potential.
+    """
+
+    pot = Potential("potentials/bump.cfg")
+
+    params = [(2,1,-15,100),
+              (1e5,1e3,-1234.,1e5),
+              (1./3,1./6,-10,5),
+              (np.pi,np.pi/2., -np.sqrt(2),23)]
+
+
+    for a, w, V0, N in params:
+        pot.adjust(a=a,w=w,v0=V0)
+        x=w+(a-w)/2.
+        xa= np.linspace(-a,a,N)
+        assert pot(x) == 0. 
+        assert pot(3./4*w) == V0
+        assert len(pot(xa))== N
+        assert pot(-5.*a) == 0.
+        assert pot(-w) == V0
+        assert pot(a) == 0.
+        with pytest.raises(ValueError):
+         pot("a")
+
+
+def test_kp():
+    """Tests the bump in the square well potential.
+    """
+
+    pot = Potential("potentials/kp.cfg")
+
+    #pot.adjust(n=n,w=w,b=b,v0=V0)
+    assert pot(-pot.params["b"]/2)== pot.params["v0"]
+    assert pot(pot.params["b"]/2)== 0.
+    assert pot(pot.params["w"])== 0.
+    
+    with pytest.raises(ValueError):
+         pot("a")
+
